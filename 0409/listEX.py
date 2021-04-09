3014873daa5acf7a596dfb8167e4e7fd5da77c51
ksj020603 <80086109+ksj020603@users.noreply.github.com>
2021-04-09 15:50:06 +0900

    Add files via upload

---
+++ 0409/listEX.py
@@ -0,0 +1,10 @@
+fstname = ['K', 'i', 'm']
+sndName = ["Se", "Jun"]
+stdNum = [2, 0, 2, 1, 0, 6, 0, 0, 7]
+
+print(fstName)
+print(sndName)
+# 화면에 K, S, J를 출력하는 프로그램을 작성하시오.
+print(fstName[0])   #K
+print(sndName[0][0])#S
+print(sndName[1][0])#J
