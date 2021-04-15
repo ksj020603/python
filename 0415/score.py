d2c2ba2d5e6ee0b6162d612103263611fce99dc1
ksj020603 <80086109+ksj020603@users.noreply.github.com>
2021-04-15 16:57:05 +0900

    Add files via upload

---
+++ 0415/score.py
@@ -0,0 +1,7 @@
+score = int(input("성적 입력 : "))
+if(score >= 60):                    #( ) 안의 결과가 True이면
+        print("합격입니다.")
+        print("축하합니다.")
+else:                               #( ) 안의 결과가 False이면
+        print("불합격입니다.")
+        print("안타깝습니다.")
