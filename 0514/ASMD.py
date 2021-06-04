672af6a5fa01c5b94a8a2c139a772d7350a21568
ksj020603 <80086109+ksj020603@users.noreply.github.com>
2021-06-04 13:29:07 +0900

    Add files via upload

---
+++ 0514/ASMD.py
@@ -0,0 +1,47 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def add(a, b):\n",
+    "    return a + b\n",
+    "def sub(a, b):\n",
+    "    return a - b\n",
+    "def mul(a, b):\n",
+    "    return a * b\n",
+    "def div(a, b):\n",
+    "    return a / b\n",
+    "a = int(input(\"첫 번째 정수 입력\"))\n",
+    "b = int(input(\"두 번째 정수 입력\"))\n",
+    "print(add(a, b))\n",
+    "print(sub(a, b))\n",
+    "print(mul(a, b))\n",
+    "print(div(a, b))"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
---
+++ 0514/examTurtle01.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 14 15:20:19 2021
+
+@author: Mac_1
+"""
+
+import turtle as t
+t.clear()
+t.shape('classic')
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
